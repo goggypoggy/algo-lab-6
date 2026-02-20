e7eb7d37976511d9bdf2e0b1694e7f51bf0faa5b
Georgiy Pogrebitskiy <goshik200712@gmail.com>
2026-02-20 18:44:09 +0300

    Task A sucks

--- a.cpp
+++ a.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <list>
+#include <stack>
 #include <vector>
 
 struct Graph {
@@ -9,16 +11,56 @@ struct Graph {
     std::vector<int> cc;
 };
 
-void DFS_Launch(Graph& G, int cur_node, int cur_cc) {
-    G.cc[cur_node] = cur_cc;
+enum class COLOR {
+    WHITE,
+    GRAY,
+    BLACK
+};
 
-    for (int next_node : G.neighbours[cur_node]) {
-        DFS_Launch(G, next_node, cur_cc);
+std::stack<int, std::list<int>> v;
+std::vector<COLOR> color;
+std::vector<int> nb_mem;
+
+void DFS_Launch(Graph& G, int start_node, int cur_cc) {
+    for (int i = 0; i < G.V; ++i) {
+        color[i] = COLOR::WHITE;
+        nb_mem[i] = 0;
+    }
+    
+    v.push(start_node);
+    color[start_node] = COLOR::GRAY;
+
+    while (!v.empty()) {
+        int top = v.top();
+        G.cc[top] = cur_cc;
+        bool added_new = false;
+
+        while (nb_mem[top] < G.neighbours[top].size()) {
+            int new_neighbor = G.neighbours[top][nb_mem[top]];
+            
+            if (color[new_neighbor] == COLOR::WHITE) {
+                v.push(new_neighbor);
+                color[new_neighbor] = COLOR::GRAY;
+                added_new = true;
+                break;   
+            }
+
+            ++nb_mem[top];
+        }
+
+        if (!added_new) {
+            color[top] = COLOR::BLACK;
+            v.pop();
+        }
     }
 }
 
 int DFS_CC(Graph& G) {
     int total_cc = 0;
+
+    color.resize(G.V);
+    nb_mem.resize(G.V);
+
     for (int i = 0; i < G.V; ++i) {
         if (G.cc[i] == 0) {
             ++total_cc;
@@ -34,9 +76,12 @@ int main(int, char**) {
 
     std::cin >> G.V >> G.E;
 
+    G.neighbours.resize(G.V);
+    G.cc.resize(G.V);
+
     for (int i = 0; i < G.V; ++i) {
-        G.neighbours.push_back({});
-        G.cc.push_back(0);
+        G.neighbours[i] = {};
+        G.cc[i] = 0;
     }
 
     for (int i = 0; i < G.E; ++i) {
@@ -45,7 +90,8 @@ int main(int, char**) {
 
         std::cin >> from >> to;
 
-        G.neighbours[from - 1].push_back(to);
+        G.neighbours[from - 1].push_back(to - 1);
+        G.neighbours[to - 1].push_back(from - 1);
     }
 
     std::cout << DFS_CC(G);
